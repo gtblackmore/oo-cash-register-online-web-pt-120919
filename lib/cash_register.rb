9f043e3175fa3e2ba1a9edd7dcf03eb53ed8fdef
George Blackmore <gtblackmore@gmail.com>
2020-01-05 18:49:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+class CashRegister
 
+  attr_accessor :total
+  
+  def initialize
+    @total = 0
+    
+  end
+  
+  def total
+  end
+  
+  def add_item
+  end
+  
+  def apply_discount
+  end 
+  
+  def items
+  end
+  
+  def void_last_transaction
+  end
+  
+end
