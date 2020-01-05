b37b2859e5656dde114b49adfed0e07d1104a797
George Blackmore <gtblackmore@gmail.com>
2020-01-05 19:18:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   
   def add_item(title, price, quantity=1)
     @total += quantity * price
-    quantity.times{@items << title}
+    quantity.times {@items << title}
   end
   
   def apply_discount
